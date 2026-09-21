3096a43daab8138e83ab7903b36982ec94e988df
cdnvidey237-dev <cdnvidey237@gmail.com>
2026-09-21 10:49:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const VIDEO_URL = "https://cdn2.videy.co/uFt7Cch71.mp4";
+    const VIDEO_URL = "https://cdn2.videy.co/hgVD4zy81.mp4";
 
     return new Response(`<!DOCTYPE html>
 <html lang="id">
@@ -28,7 +28,8 @@ export default {
       background: #000;
     }
 
-    .video-box {
+    .video-container {
+      position: relative;
       width: 100%;
       max-width: 640px;
       margin: 0 auto;
@@ -40,34 +41,61 @@ export default {
       display: block;
     }
 
-    .ad-box {
-      width: 100%;
+    /* IKLAN DI TENGAH VIDEO */
+    .ad-overlay {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 728px;
+      height: 90px;
+      z-index: 10;
       display: flex;
       justify-content: center;
+      align-items: center;
       overflow: hidden;
-      margin-top: 15px;
+    }
+
+    @media (max-width: 728px) {
+      .ad-overlay {
+        width: 100%;
+        height: 90px;
+      }
     }
   </style>
 
   <!-- JuicyAds v3.0 -->
-  <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+  <script
+    type="text/javascript"
+    data-cfasync="false"
+    async
+    src="https://poweredby.jads.co/js/jads.js">
+  </script>
 </head>
 
 <body>
 
-  <div class="video-box">
+  <div class="video-container">
+
     <video controls playsinline preload="metadata">
       <source src="${VIDEO_URL}" type="video/mp4">
       Browser kamu tidak mendukung video.
     </video>
-  </div>
 
-  <!-- JuicyAds Leaderboard 728x90 -->
-  <div class="ad-box">
-    <ins id="1127279" data-width="728" data-height="90"></ins>
-    <script type="text/javascript" data-cfasync="false" async>
-      (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1127279});
-    </script>
+    <!-- JuicyAds Leaderboard 728x90 -->
+    <div class="ad-overlay">
+      <ins id="1127279" data-width="728" data-height="90"></ins>
+
+      <script
+        type="text/javascript"
+        data-cfasync="false"
+        async>
+        (adsbyjuicy = window.adsbyjuicy || []).push({
+          'adzone': 1127279
+        });
+      </script>
+    </div>
+
   </div>
 
 </body>
