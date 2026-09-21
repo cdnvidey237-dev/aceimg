7f9f19bbe2417b90d2148126677dd1890524c4db
cdnvidey237-dev <cdnvidey237@gmail.com>
2026-09-21 10:29:43 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const VIDEO_URL = "https://cdn2.videy.co/hgVD4zy81.mp4";
+    const VIDEO_URL = "https://cdn2.videy.co/uFt7Cch71.mp4";
 
     return new Response(`<!DOCTYPE html>
 <html lang="id">
@@ -11,7 +11,6 @@ export default {
   <!-- JuicyAds verification -->
   <meta name="juicyads-site-verification" content="0c2ef4163313b08e5965d2436a5760b0">
 
-  <!-- Hilangkan favicon/logo lama -->
   <link rel="icon" href="data:,">
 
   <title>Video</title>
@@ -40,16 +39,37 @@ export default {
       height: auto;
       display: block;
     }
+
+    .ad-box {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      overflow: hidden;
+      margin-top: 15px;
+    }
   </style>
+
+  <!-- JuicyAds v3.0 -->
+  <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
 </head>
 
 <body>
+
   <div class="video-box">
     <video controls playsinline preload="metadata">
       <source src="${VIDEO_URL}" type="video/mp4">
       Browser kamu tidak mendukung video.
     </video>
   </div>
+
+  <!-- JuicyAds Leaderboard 728x90 -->
+  <div class="ad-box">
+    <ins id="1127279" data-width="728" data-height="90"></ins>
+    <script type="text/javascript" data-cfasync="false" async>
+      (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1127279});
+    </script>
+  </div>
+
 </body>
 </html>`, {
       headers: {
